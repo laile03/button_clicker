55eeeb1b1d0ae429c820f780543f3db99955ec89
Lai <laile110395@gmail.com>
2021-07-26 23:55:08 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("testing");
+
+function login(element) {
+    console.log(element);
+    element.innerText = "Logout";
+}
+
+function hide(element){
+    element.remove()
+}
+
+function message(){
+    alert("Ninja was liked")
+}
